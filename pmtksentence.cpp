84cfeb4d6527c12d93b0c1a564bc18cbe193827d
fatihsezgin <fatihsezgin98@hotmail.com>
2020-02-04 10:04:57 +0300

    Status for PMTK commands handling has done

---
+++ pmtksentence.cpp
@@ -0,0 +1,6 @@
+#include "pmtksentence.h"
+
+PMTKSentence::PMTKSentence(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ pmtksentence.h
@@ -0,0 +1,17 @@
+#ifndef PMTKSENTENCE_H
+#define PMTKSENTENCE_H
+
+#include <QObject>
+
+class PMTKSentence : public QObject
+{
+    Q_OBJECT
+public:
+    explicit PMTKSentence(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // PMTKSENTENCE_H
